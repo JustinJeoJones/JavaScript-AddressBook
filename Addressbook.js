461e63684a19aded565a2ed319c04cb332ac252d
JustinJonesGC <Justin@grandcircus.co>
2018-05-08 13:37:18 -0400

    third commit. Fixed code to use the class more

--- Addressbook.js
+++ Addressbook.js
@@ -7,7 +7,7 @@ class contact {
         this.relation = relation;
     }
     toString() {
-        return "Name:" +this.name + "<br> Email:" + this.email + "<br> Phone:" + this.phone + "<br> Relation:" + this.relation + "<br>"; //formats the return to have spaces between the variables
+        return "Name:" +this.name + "<br> Email:" + this.email + "<br> Phone:" + this.phone + "<br> Relation:" + this.relation + "<br> "; //formats the return to have spaces between the variables
     }
 }
 
@@ -15,13 +15,20 @@ class AddressBook {
     constructor() {
         this.contacts = [];
     }
-    add(name, email, phone, relation) { //Adds new contact to the book
-        var newContact = new contact(name, email, phone, relation);
+    add(newContact){
         this.contacts.push(newContact);
-        Book.print(newContact);
     }
     deleteAt(ind) { // Deletes contact based on position (ind)
         this.contacts.splice(ind,1);
+    }
+    deleteName(inputName){
+        let x;
+        this.contacts.forEach(function(person,ind){
+            if (person.name == inputName){
+                x = ind;
+            }
+        });
+        this.contacts.splice(x,1);
     }
 }
 
@@ -30,6 +37,7 @@ var count=0;
 
 function addContacts(){
     var thisContact = new contact(document.getElementById("name").value,document.getElementById("email").value,document.getElementById("phone").value,document.getElementById("relation").value);
+    Book.add(thisContact);
     // create a p element for inserting in el
     var el = document.querySelector('#contacts');
 
@@ -37,12 +45,15 @@ function addContacts(){
     console.log(el.innerHTML);
     count = count +1;
     // append to the element's content
-    el.innerHTML +='<p id="'+count+'">' + thisContact.toString() + '<button type="button" onclick="removeElement('+count+')">Delete</button></p>';
+    el.innerHTML +='<p id="'+count+'">' + thisContact.toString() + '<button type="button" onclick="removeElement('+count+')">Delete</button> <input type="hidden" class ="hidden" id="h'+count+'" value="'+thisContact.name+'"></p>';
 
 }
 
 function removeElement(deleteSpot) {
     // Removes an element from the document
+    var x ="h"+deleteSpot;
+    var deleteName = document.getElementById(x).value;
+    Book.deleteName(deleteName);
     var el = document.getElementById(deleteSpot);
     el.parentNode.removeChild(el);
 }
